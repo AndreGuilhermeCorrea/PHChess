613d0bad32e98aeadbc384eb1343461710809291
AndreGuilhermeCorrea <de_correa@msn.com>
2024-11-01 12:16:49 -0300

    🎉 feat: footer and header

---
+++ public/template/footer.php
@@ -0,0 +1,6 @@
+    </main>
+    <footer>
+        <p>Chess Game by<a href="mailto:andreguilhermecorrea@example.com">andreguilhermecorrea&copy; 2024</a></p>
+    </footer>
+</body>
+</html>
---
+++ public/template/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHChess</title>
+      <!-- Google fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1>PHChess</h1>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="registro.php">Registre-se</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="sobre.php">Sobre</a></li>
+                    <li><a href="contact.php">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
